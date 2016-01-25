7254061a5fe2638899475a2421e4d147b252a757
Tomasz Szadkowski <tszad@cleeng.com>
2016-01-25 15:22:21 +0100

    general cleanup

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -7,10 +7,7 @@ return [
         ],
         'factories' => [
             App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
-            App\Action\AuthenticateAction::class => App\Action\AuthenticateFactory::class,
             App\Action\PremiumContentAction::class => App\Action\PremiumContentFactory::class,
-            App\Action\SelectPlanAction::class => App\Action\SelectPlanFactory::class,
-            App\Action\SettingsAction::class => App\Action\SettingsFactory::class,
         ],
     ],
 
@@ -22,27 +19,9 @@ return [
             'allowed_methods' => ['GET'],
         ],
         [
-            'name' => 'authenticate',
-            'path' => '/authenticate',
-            'middleware' => App\Action\AuthenticateAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
             'name' => 'premium-content',
             'path' => '/premium-content',
             'middleware' => App\Action\PremiumContentAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'select-plan',
-            'path' => '/select-plan',
-            'middleware' => App\Action\SelectPlanAction::class,
-            'allowed_methods' => ['GET'],
-        ],
-        [
-            'name' => 'setting',
-            'path' => '/settings',
-            'middleware' => App\Action\SettingsAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
